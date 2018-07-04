c10c96d6b341c3bede9a30974928648d8f38d730
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-04 12:10:48 +0200

    progress bar is added to form and works.

--- TimerEvents/Form1.cs
+++ TimerEvents/Form1.cs
@@ -12,9 +12,25 @@ namespace TimerEvents
 {
     public partial class Form1 : Form
     {
+        System.Windows.Forms.Timer t;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new System.Windows.Forms.Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 10;
+            if (progressBar1.Value >= 100) t.Stop();
         }
     }
 }
